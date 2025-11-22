ca30649dc6f37469fde65892419c074062a3eaf6
wandilemawelela <wandile1997@gmail.com>
2025-11-22 20:48:07 +0200

    Java Control Structures

---
+++ Java/Unit 2/Example.java
@@ -0,0 +1,12 @@
+public class Example {
+	public static void main(String[] args) {
+		int number;
+		number = 1;
+
+		while (number < 6) {
+			System.out.println(number);
+			number = number + 1;
+	}
+	System.out.println("Done!");
+	}
+}
